bd9b133813703067eb6bb36dacd51e24cd044fbb
hyevvy <hyeg0@naver.com>
2023-12-11 09:34:20 +0900

    design: login layout 모달 제거

--- src/app/(loginLayout)/layout.tsx
+++ src/app/(loginLayout)/layout.tsx
@@ -1,12 +1,11 @@
 import { ReactNode } from "react";
-import styles from '@/app/page.module.css';
+import styles from "@/app/page.module.css";
 
-export default function LoginLayout({ children, modal }: { children: ReactNode, modal: ReactNode }) {
-    return (
-        <div className={styles.container}>
-            LOGIN LAYOUT
-            {children}
-            {modal}
-        </div>
-    )
+export default function LoginLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={styles.container}>
+      LOGIN LAYOUT
+      {children}
+    </div>
+  );
 }
