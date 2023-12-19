476ee32d2bf9b996c9112f95b2028b2e8f2dd8c0
hyevvy <hyeg0@naver.com>
2023-12-19 12:04:43 +0900

    design: membership 소개 페이지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import styles from "@/app/page.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
