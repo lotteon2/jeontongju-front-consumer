af5de198b02f6125cdb894489caa353f7472120e
hyevvy <hyeg0@naver.com>
2023-12-21 10:40:21 +0900

    feat: 결제 실패 페이지

--- src/apis/payment/paymentAPIService.ts
+++ src/apis/payment/paymentAPIService.ts
@@ -1,5 +1,6 @@
 import { authAxiosInstance } from "../common";
 import {
+  BuyCreditParams,
   KakaoParams,
   MembershipParams,
   PayResponseData,
@@ -16,6 +17,13 @@ const paymentAPI = {
   membership: async (params: MembershipParams) => {
     const { data } = await authAxiosInstance.post<PayResponseData>(
       `/payment-service/api/subscription`,
+      params
+    );
+    return data;
+  },
+  buyCredit: async (params: BuyCreditParams) => {
+    const { data } = await authAxiosInstance.post<PayResponseData>(
+      `/payment-service/api/credit`,
       params
     );
     return data;
