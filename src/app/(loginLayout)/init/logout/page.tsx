262361cb2e4ce06ec9e28b4012c2dd95aaed4f82
hyevvy <hyeg0@naver.com>
2023-12-26 18:17:33 +0900

    feat: mypage 로그인한 유저만 접근할 수 있도록 수정

--- src/app/(loginLayout)/init/logout/page.tsx
+++ src/app/(loginLayout)/init/logout/page.tsx
@@ -1,11 +1,14 @@
 "use client";
+import { useMyInfoStore } from "@/app/store/myInfo/myInfo";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Logout() {
   const router = useRouter();
+  const [setIsLogin] = useMyInfoStore((state) => [state.dispatchIsLogin]);
   useEffect(() => {
     window.localStorage.removeItem("accessToken");
+    setIsLogin(false);
     router.replace("/");
   }, []);
   return <></>;
--- src/app/(mainLayout)/mypage/page.tsx
+++ src/app/(mainLayout)/mypage/page.tsx
@@ -1,8 +1,22 @@
 "use client";
+import { useMyInfoStore } from "@/app/store/myInfo/myInfo";
 import MyInfoBox from "../_component/MyPage/MyInfoBox/MyInfoBox";
 import MyOrderList from "../_component/MyPage/MyOrderList";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function MyPage() {
+  const router = useRouter();
+
+  const [isLogin] = useMyInfoStore((state) => [state.isLogin]);
+
+  useEffect(() => {
+    if (!isLogin) {
+      toast("로그인한 유저만 접근할 수 있어요.");
+      router.push("/404");
+    }
+  });
   return (
     <div>
       <MyInfoBox />
