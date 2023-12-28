08fe82b744e1e247de7038c92e263a6f163ef9e3
hyevvy <hyeg0@naver.com>
2023-12-28 16:40:12 +0900

    feat: 알림 읽음처리

--- src/app/(mainLayout)/_component/Noti/Noti.tsx
+++ src/app/(mainLayout)/_component/Noti/Noti.tsx
@@ -10,7 +10,6 @@ import { toast } from "react-toastify";
 function Noti() {
   const [newNoti, setNewNoti] = useState<string[]>([]);
   const [notiOpen, setNotiOpen] = useState<boolean>(false);
-  const [animationClass, setAnimationClass] = useState(styles["slide-in"]);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -54,40 +53,7 @@ function Noti() {
         eventSource.close();
         console.log("SSE CLOSED");
       };
-
-      // eslint-disable-next-line
-      // eventSource.addEventListener("connect", (event: any) => {
-      //   console.log(event);
-      //   const { data: receivedConnectData } = event;
-      //   if (receivedConnectData === "SSE 연결이 완료되었습니다.") {
-      //     console.log("SSE CONNECTED");
-      //   } else {
-      //     console.log(event);
-      //   }
-      // });
-
-      // eslint-disable-next-line
-      // eventSource.addEventListener("happy", (event: any) => {
-      //   console.log(event);
-      //   const newNoti = event.data;
-      //   console.log("HI");
-      //   setNewNoti((prev) => [...prev, newNoti]);
-      //   setAnimationClass(styles["slide-in"]); // 슬라이드 애니메이션
-
-      //   // 5초 후에 알림 언마운트하고 상태 비우기
-      //   const slideOutTimer = setTimeout(() => {
-      //     setAnimationClass(styles["slide-out"]);
-      //   }, 5000);
-
-      //   return () => clearTimeout(slideOutTimer);
-      // });
-
-      // return () => {
-      //   eventSource.close();
-      //   console.log("SSE CLOSED");
-      // };
     }
-    // eslint-disable-next-line
   }, []);
 
   const handleOpenNoti = () => {
@@ -99,6 +65,7 @@ function Noti() {
       const data = await notificationAPI.readAllNoti();
       if (data.code === 200) {
         toast("전체 읽음 처리에 성공했어요.");
+        setNewNoti([]);
       }
     } catch (error) {
       toast("전체 읽음 처리에 실패했어요");
