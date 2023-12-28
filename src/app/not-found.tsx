7d509308652d84510273479def7981efaa6ab5c5
hyevvy <hyeg0@naver.com>
2023-12-28 15:53:33 +0900

    design: 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,29 @@
+"use client";
+import { useRouter } from "next/navigation";
+import not_found from "/public/not_found.mp4";
 import { NextPage } from "next";
 
 const NotFound: NextPage = () => {
+  const router = useRouter();
   return (
-    <div>
-      <div>이 페이지는 존재하지 않아요. 다른 페이지를 검색해보세요.</div>
+    <div
+      onClick={() => router.push("/")}
+      style={{ width: "70vw", margin: "0 auto" }}
+    >
+      <video
+        src={not_found}
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        width={0}
+        height={0}
+        style={{
+          width: "100%",
+          height: "80%",
+          cursor: "pointer",
+          borderRadius: "5px",
+        }}
+      />
     </div>
   );
 };
