063a6d00d16c8f629d388579d4a5efc22e86476c
hyevvy <hyeg0@naver.com>
2023-12-29 10:01:42 +0900

    test: 중간 커밋

--- src/app/(mainLayout)/product/[productId]/page.tsx
+++ src/app/(mainLayout)/product/[productId]/page.tsx
@@ -106,8 +106,8 @@ export default function Page({ params }: Props) {
             <div className={style.info}>
               <div className={style.title}>
                 <div>{productData.productName}</div>
-                {/* {productData.registeredQuantity < 10 && ( */}
-                <div style={{ color: "red" }}>품절 임박</div>
+                {productData.registeredQuantity < 10 && (
+                  <div style={{ color: "red" }}>품절 임박</div>
                 )}
               </div>
               <div className={style.desc}>{productData.productDescription}</div>
