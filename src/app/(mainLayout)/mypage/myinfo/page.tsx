26b8453fb3e246f097f92952f78cb91da31f1f8c
hyevvy <hyeg0@naver.com>
2024-01-03 12:33:46 +0900

    feat: 비로그인시 myinfo 페이지 접근 제한

--- src/app/(mainLayout)/mypage/myinfo/page.tsx
+++ src/app/(mainLayout)/mypage/myinfo/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 import consumerAPI from "@/apis/consumer/consumerAPIService";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { toast } from "react-toastify";
 
 export default function MyInfo() {
   const router = useRouter();
-
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (!localStorage.getItem("accessToken")) {
+        toast("로그인한 유저만 접근할 수 있어요.");
+        router.replace("/init/signin");
+      }
+    }
+  }, []);
   //TODO : alert
   const handleWithDrawal = async () => {
     try {
