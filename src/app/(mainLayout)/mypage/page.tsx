0d7a5c42e9d8fce110f8191bd1058169d54b470e
hyevvy <hyeg0@naver.com>
2024-01-06 12:51:25 +0900

    feat: mypage leftbar

--- src/app/(mainLayout)/mypage/page.tsx
+++ src/app/(mainLayout)/mypage/page.tsx
@@ -8,6 +8,8 @@ import MyWishList from "./_component/MyWishList/MyWishList";
 import { QueryClient } from "@tanstack/react-query";
 import MyCartList from "./_component/MyCartList/MyCartList";
 import MyReviewList from "./_component/MyReviewList/MyReviewList";
+import { Anchor } from "antd";
+import style from "@/app/(mainLayout)/mypage/mypage.module.css";
 
 export default function MyPage() {
   const router = useRouter();
@@ -20,12 +22,51 @@ export default function MyPage() {
   });
 
   return (
-    <div>
+    <div className={style.myPage}>
       <MyInfoBox />
-      <MyWishList />
-      <MyOrderList />
-      <MyCartList />
-      <MyReviewList />
+      <div className={style.myPageBody}>
+        <Anchor
+          className={style.leftBar}
+          affix={false}
+          items={[
+            {
+              key: "1",
+              href: "#myWish",
+              title: "나의 찜 내역",
+            },
+            {
+              key: "2",
+              href: "#myOrder",
+              title: "나의 주문 내역",
+            },
+            {
+              key: "3",
+              href: "#myCart",
+              title: "나의 장바구니 내역",
+            },
+            {
+              key: "3",
+              href: "#myReview",
+              title: "나의 리뷰 내역",
+            },
+          ]}
+        />
+        <div className={style.rightBar}>
+          <div id="myWish">
+            <MyWishList />
+          </div>
+          <div id="myOrder">
+            <MyOrderList />
+          </div>
+          <div id="myCart">
+            <MyCartList />
+          </div>
+
+          <div id="myReview">
+            <MyReviewList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
