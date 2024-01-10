42ad610d0b1d89aa18cf507bc77bf47ae5e1d698
hyevvy <hyeg0@naver.com>
2024-01-10 17:35:58 +0900

    bug: build 오류 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,13 @@ import { Jua } from "next/font/google";
 import styles from "@/app/page.module.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 const jua = Jua({ subsets: ["latin"], display: "swap", weight: "400" });
 
 export const metadata: Metadata = {
   title: "전통주점",
   description: "전통주, 마침표를 찍다.",
-  manifest: "/manifest.json",
+  manifest: "./manifest.json",
 };
 
 export default function RootLayout({
