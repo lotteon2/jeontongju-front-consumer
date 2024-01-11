7eb82c1e2e717b3162209793e38a7506dbd0856e
hyevvy <hyeg0@naver.com>
2024-01-11 21:49:07 +0900

    feat: credit 결제

--- src/app/(mainLayout)/credit/list/page.tsx
+++ src/app/(mainLayout)/credit/list/page.tsx
@@ -7,8 +7,13 @@ import consumerAPI from "@/apis/consumer/consumerAPIService";
 import { Credit, Trade } from "@/apis/consumer/consumerAPIservice.types";
 import CreditBox from "../../_component/PointCreditBox/PointCreditBox";
 import { toast } from "react-toastify";
+import { Button, Input } from "antd";
+import paymentAPI from "@/apis/payment/paymentAPIService";
+import { useRouter } from "next/navigation";
 export default function CreditList() {
-  const [type, setType] = useState<"charge" | "bid">("bid");
+  const router = useRouter();
+  const [userInputMoney, setUserInputMoney] = useState<number>(0);
+  const [type, setType] = useState<"charge" | "bid">("charge");
   const [currentCredit, setCurrentCredit] = useState<number>();
   const [accCredit, setAccCredit] = useState<number>();
   const [useCredit, setUseCredit] = useState<number>();
@@ -42,6 +47,26 @@ export default function CreditList() {
     console.log("credit", credits);
   }, [type, page, size]);
 
+  const handleBuyCredit = async (money: number) => {
+    const params = {
+      chargeCredit: money,
+      paymentType: "CREDIT",
+      paymentMethod: "KAKAO",
+      itemName: `크레딧 ${money}원`,
+    };
+
+    try {
+      const data = await paymentAPI.buyCredit(params);
+      if (data.message) {
+        console.error(data.message);
+      } else {
+        router.replace(data.next_redirect_pc_url);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       {mounted && !isLoading ? (
@@ -52,6 +77,24 @@ export default function CreditList() {
               <div>현재 내 크레딧 | {currentCredit}</div>
               <div>총 적립 크레딧 | {accCredit}</div>
               <div>총 사용 크레딧 | {useCredit}</div>
+            </div>
+          </div>
+          <div>
+            <div>크레딧 충전하기</div>
+            <div className={style.credits}>
+              <Button onClick={() => handleBuyCredit(10000)}>만원</Button>
+              <Button onClick={() => handleBuyCredit(50000)}>오만원</Button>
+              <Button onClick={() => handleBuyCredit(100000)}>십만원</Button>
+              <Button onClick={() => handleBuyCredit(1000000)}>백만원</Button>
+              <div className={style.credits}>
+                <Input
+                  value={userInputMoney}
+                  onChange={(e) => setUserInputMoney(Number(e.target.value))}
+                />
+                <Button onClick={() => handleBuyCredit(userInputMoney)}>
+                  결제
+                </Button>
+              </div>
             </div>
           </div>
           <div className={style.creditBtns}>
