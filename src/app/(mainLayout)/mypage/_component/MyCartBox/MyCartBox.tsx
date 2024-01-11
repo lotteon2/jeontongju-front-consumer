446ec3a6e93fbcc1bdd05f60b039ef27c37d2033
hyevvy <hyeg0@naver.com>
2024-01-11 13:49:01 +0900

    feat: 장바구니페이지 수량 변경

--- src/app/(mainLayout)/mypage/_component/MyCartBox/MyCartBox.tsx
+++ src/app/(mainLayout)/mypage/_component/MyCartBox/MyCartBox.tsx
@@ -1,8 +1,11 @@
+"use client";
+import FiSrCoffee from "/public/fi-sr-coffee.svg";
 import wishAPI from "@/apis/wishCart/wishAPIService";
 import { GetMyCartListResponseData } from "@/apis/wishCart/wishAPIService.types";
 import Image from "next/image";
 import { toast } from "react-toastify";
 import style from "@/app/(mainLayout)/mypage/_component/MyCartBox/MyCartBox.module.css";
+import { useState } from "react";
 
 export default function MyCartBox({
   item,
@@ -21,8 +24,41 @@ export default function MyCartBox({
     } catch (error) {}
   };
 
+  const [quantity, setQuantity] = useState(item.amount);
+  const [total, setTotal] = useState(item.amount * item.productPrice);
+
+  const handleClickCounter = async (num: number) => {
+    setQuantity((prev) => (prev as number) + num);
+    setTotal((prev) => prev + item.productPrice * num);
+    const data = await wishAPI.updateCart(item.productId, item.amount);
+    if (data.code !== 200) {
+      toast("수량 수정에 실패했어요.");
+    }
+  };
+
+  const handleBlurInput = (quantity: number) => {
+    const newQuantity = quantity;
+    setQuantity(newQuantity);
+    setTotal(item.productPrice * newQuantity);
+  };
+
   return (
     <div className={style.myCartBox}>
+      <div>
+        <Image
+          alt="삭제"
+          width={0}
+          height={0}
+          src={FiSrCoffee}
+          style={{
+            cursor: "pointer",
+            width: "1rem",
+            height: "1rem",
+            position: "relative",
+          }}
+          onClick={handleDeleteCartItem}
+        />
+      </div>
       <div className={style.myCartBody}>
         <div style={{ width: "10rem", height: "10rem", position: "relative" }}>
           <Image
@@ -38,8 +74,14 @@ export default function MyCartBox({
         </div>
       </div>
       <div>
-        <div>{item.amount}</div>
-        <div onClick={handleDeleteCartItem}>지우기</div>
+        <QualityInput
+          quantity={quantity}
+          stock={100}
+          onClick={handleClickCounter}
+          setQuantity={setQuantity}
+          onBlur={handleBlurInput}
+        />
+        <div>{total}</div>
       </div>
     </div>
   );
