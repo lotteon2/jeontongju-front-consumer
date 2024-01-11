0209926ab68185f14ddc15ec7bdf825f3c782b6a
hyevvy <hyeg0@naver.com>
2024-01-11 15:39:21 +0900

    feat: login logo 추가

--- src/app/(loginLayout)/layout.tsx
+++ src/app/(loginLayout)/layout.tsx
@@ -1,20 +1,31 @@
+"use client";
+import logoImg from "/public/logo.png";
 import styles from "@/app/page.module.css";
 import Script from "next/script";
 import Link from "next/link";
 import NextServer from "next/script";
 import Footer from "../(mainLayout)/_component/Footer/Footer";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function LoginLayout({
   children,
 }: {
   children: React.ReactNode | typeof NextServer;
 }) {
+  const router = useRouter();
   return (
     <div className={styles.loginContainer}>
       <header className={styles.loginHeader}>
-        <Link href="/"></Link>
-        <a href="/">LOGO</a>
+        <div onClick={() => router.push("/")}>
+          <Image
+            src={logoImg}
+            width={0}
+            height={0}
+            alt="loading"
+            style={{ cursor: "pointer", width: "5rem", height: "5rem" }}
+          />
+        </div>
       </header>
       {children}
       <Footer />
