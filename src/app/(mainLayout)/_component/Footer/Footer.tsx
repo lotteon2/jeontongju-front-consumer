b195f85084e5c4f0bc50fdbf402796045e448f44
hyevvy <hyeg0@naver.com>
2024-01-12 09:22:26 +0900

    bug: 빌드 오류 해결

--- src/app/(mainLayout)/_component/Footer/Footer.tsx
+++ src/app/(mainLayout)/_component/Footer/Footer.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
+"use client";
 import logoImg from "/public/logo.png";
+import Image from "next/image";
 import style from "@/app/(mainLayout)/_component/Footer/Footer.module.css";
 export default function Footer() {
   return (
