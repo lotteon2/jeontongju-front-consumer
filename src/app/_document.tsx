186429db4f5f7597f2d19ce2bfea0c73cac608c2
hyevvy <hyeg0@naver.com>
2024-01-18 10:00:28 +0900

    test: build 오류 해결

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -14,6 +14,17 @@ declare global {
 }
 
 export default function Document() {
+  if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+    process.on("SIGTERM", () => {
+      console.log("Received SIGTERM: cleaning up");
+      process.exit(0);
+    });
+    process.on("SIGINT", () => {
+      console.log("Received SIGINT: cleaning up");
+      process.exit(0);
+    });
+  }
+
   return (
     <Html>
       <Head>
