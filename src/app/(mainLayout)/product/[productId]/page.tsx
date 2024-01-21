9dea74c1e25957262125a93836546e89db6a9a1f
hyevvy <hyeg0@naver.com>
2024-01-21 15:00:11 +0900

    feat: productImage Img

--- src/app/(mainLayout)/product/[productId]/page.tsx
+++ src/app/(mainLayout)/product/[productId]/page.tsx
@@ -247,8 +247,8 @@ export default function Page({ params }: Props) {
                     src={productData.productDetailsImageUrl}
                     alt="productDetailImg"
                     priority
-                    width={100}
-                    height={100}
+                    width={1000}
+                    height={1000}
                     style={{
                       cursor: "pointer",
                       width: "70%",
