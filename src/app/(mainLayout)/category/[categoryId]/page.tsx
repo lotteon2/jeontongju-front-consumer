5fe04c64ce6222afd5786143cbd44ab407234db5
hyevvy <hyeg0@naver.com>
2024-01-29 09:28:45 +0900

    feat: category sort에 따른 정렬

--- src/app/(mainLayout)/category/[categoryId]/page.tsx
+++ src/app/(mainLayout)/category/[categoryId]/page.tsx
@@ -64,6 +64,7 @@ export default function CategoryPage({ params }: Props) {
         maxAlcoholDegree,
         concepts,
         categoryId,
+        sort
       ],
       queryFn: ({ pageParam = 0 }) =>
         searchAPI.getAllProductListForCategoryId(
